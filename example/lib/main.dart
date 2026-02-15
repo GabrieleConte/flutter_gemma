0e81d9e38c3471c0803f508f49db3e3988f2cc25
GabrieleConte <gabryconte90@gmail.com>
2026-02-15 22:25:54 +0100

    modified main entrypoint

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gemma/flutter_gemma.dart';
-import 'package:flutter_gemma_example/home_screen.dart';
+import 'package:flutter_gemma_example/graph_rag_navigator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +30,10 @@ class ChatApp extends StatelessWidget {
         ),
       ),
       themeMode: ThemeMode.dark,
-      home: const SafeArea(child: HomeScreen()),
+      home: const PopScope(
+        canPop: false,
+        child: SafeArea(child: GraphRAGNavigator()),
+      ),
     );
   }
 }
